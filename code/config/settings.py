3ab5e70ab7052781fd21f13aeaabd63099bfcf1c
Benjamin R. Luginbuhl <ben.luginbuhl@gmail.com>
2020-12-30 12:10:40 -0800

    Products chart views
    - using pandas in function-based views to render dataframes as tables

--- code/config/settings.py
+++ code/config/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +61,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+STATIC_ROOT = str(BASE_DIR.joinpath('static_cdn', 'static_root'))
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('static_cdn', 'media_root'))
---
+++ code/products/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Product, Purchase
+
+import pandas as pd
+
+
+def chart_select_view(request):
+    product_df = pd.DataFrame(Product.objects.all().values())
+    purchase_df = pd.DataFrame(Purchase.objects.all().values())
+    context = {
+        'products': product_df.to_html(),
+        'purchases': purchase_df.to_html(),
+    }
+    return render(request, 'products/main.html', context)
