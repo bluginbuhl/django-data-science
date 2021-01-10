f65d662c535146944eb1dbdf431a9977407e8749
Benjamin R. Luginbuhl <ben.luginbuhl@gmail.com>
2021-01-10 12:58:47 -0800

    Video 14 - Add error message for dataframe

--- code/products/views.py
+++ code/products/views.py
@@ -5,6 +5,9 @@ import pandas as pd
 
 
 def chart_select_view(request):
+
+    error_message = None
+
     product_df = pd.DataFrame(Product.objects.all().values())
     product_df.rename({
         'id': 'product_id',
@@ -13,19 +16,23 @@ def chart_select_view(request):
     product_df['product_id'] = product_df['id']
     purchase_df = pd.DataFrame(Purchase.objects.all().values())
 
-    df = pd.merge(purchase_df, product_df, on='product_id').drop(
-        ['id_y', 'date_y'], axis=1).rename(
-        {'id_x': 'id', 'date_x': 'date'}, axis=1)
-
-    if request.method == 'POST':
-        chart_type = request.POST['sales']
-        date_from = request.POST['date_from']
-        date_to = request.POST['date_to']
-        print(chart_type, date_from, date_to)
+    if purchase_df.shape[0] > 0:
+        df = pd.merge(purchase_df, product_df, on='product_id').drop(
+            ['id_y', 'date_y'], axis=1).rename(
+            {'id_x': 'id', 'date_x': 'date'}, axis=1)
+        if request.method == 'POST':
+            chart_type = request.POST['sales']
+            date_from = request.POST['date_from']
+            date_to = request.POST['date_to']
+            print(chart_type, date_from, date_to)
+    else:
+        df = None
+        error_message = "No records in database"
 
     context = {
+        'error_message': error_message,
         'products': product_df.to_html(),
         'purchases': purchase_df.to_html(),
-        'df': df.to_html(),
+        'df': df,
     }
     return render(request, 'products/main.html', context)
--- code/static/js/main.js
+++ code/static/js/main.js
@@ -3,4 +3,12 @@ console.log("main.js loaded")
 $(document).ready(function() {
     $('.ui.dropdown')
         .dropdown();
-})
+});
+
+$(document).ready(function() {
+    $('.message .close')
+      .on('click', function() {
+        $(this).closest('.message').transition('fade')
+        ;
+      })
+});
