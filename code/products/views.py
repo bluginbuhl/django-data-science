a7c19482401352fd916f89505e13e919e8c8857a
Benjamin R. Luginbuhl <ben.luginbuhl@gmail.com>
2021-01-10 13:21:13 -0800

    Video 17 - Group by date for purchase df

--- code/products/views.py
+++ code/products/views.py
@@ -25,13 +25,16 @@ def chart_select_view(request):
             chart_type = request.POST['sales']
             date_from = request.POST['date_from']
             date_to = request.POST['date_to']
+
+            df['date'] = df['date'].apply(lambda x: x.strftime('%Y-%m-%d'))
+            print(df['date'])
+            df2 = df.groupby('date', as_index=False)['total_price'].agg('sum')
+            print(df2)
     else:
         error_message = "No records in database"
 
     context = {
         'error_message': error_message,
-        'products': product_df.to_html(),
-        'purchases': purchase_df.to_html(),
-        'df': df,
     }
+
     return render(request, 'products/main.html', context)
