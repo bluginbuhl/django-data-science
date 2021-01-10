b4e018e7ce820657d2d4e44d09d67098b406efe9
Benjamin R. Luginbuhl <ben.luginbuhl@gmail.com>
2021-01-10 11:33:43 -0800

    Video 09 - merging two dataframes

--- code/products/views.py
+++ code/products/views.py
@@ -6,9 +6,20 @@ import pandas as pd
 
 def chart_select_view(request):
     product_df = pd.DataFrame(Product.objects.all().values())
+    product_df.rename({
+        'id': 'product_id',
+        'date': 'date_product_added',
+    })
+    product_df['product_id'] = product_df['id']
     purchase_df = pd.DataFrame(Purchase.objects.all().values())
+
+    df = pd.merge(purchase_df, product_df, on='product_id').drop
+    (['id_y', 'date_y'], axis=1).rename(
+        {'id_x': 'id', 'date_x': 'date'}, axis=1)
+
     context = {
         'products': product_df.to_html(),
         'purchases': purchase_df.to_html(),
+        'df': df.to_html(),
     }
     return render(request, 'products/main.html', context)
